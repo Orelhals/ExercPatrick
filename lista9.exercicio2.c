f370dda0c963daa3ef5d653690cec2feabad2f45
AndreLuiz-LC <152039081+AndreLuiz-LC@users.noreply.github.com>
2025-06-17 14:53:00 -0300

    Update and rename exercise_lista8_2.c to lista9.exercicio2.c
    lista 8 -> lista9

--- lista9.exercicio2.c
+++ lista9.exercicio2.c
@@ -14,9 +14,9 @@ Data aniversarios[3] = { {5,1,"JANEIRO"},
 int a;
 Data *p_dt;
 p_dt=&aniversarios[2];
-printf("Nome do mÍs %d È: %s.", p_dt->mes, p_dt->nomeMes);
-strcpy(p_dt->nomeMes, "MAR«O");
-printf("\nN˙mero de letras : %d=", strlen(p_dt->nomeMes)); // Imprime o valor de 5, pois no aniversario[2] o valor "Maio" È subscrito por "MarÁo" que tem 5 letras
+printf("Nome do m√™s %d √©: %s.", p_dt->mes, p_dt->nomeMes);
+strcpy(p_dt->nomeMes, "MAR√áO");
+printf("\nN√∫mero de letras : %d=", strlen(p_dt->nomeMes)); // Imprime o valor de 5, pois no aniversario[2] o valor "Maio" √© subscrito por "Mar√ßo" que tem 5 letras
 Data p_dt2 = (Data *) malloc(sizeof(Data)); // O correto seria Data * p_dt2 = (Data *) malloc (sizeof(Data));
 p_dt2 = &aniversario[0];
 
